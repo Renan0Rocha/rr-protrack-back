1cbdc7081c029466028f8f87ea394b572b4f97e6
Eduardo <dugabuga6@gmail.com>
2025-05-14 00:05:03 -0400

    feat: implement Cliente module, controller, and service

---
+++ src/Cliente/cliente.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
+import { clienteDto } from './cliente.dto';
+import { ClienteService } from './cliente.service';
+
+@Controller('cliente')
+export class ClienteController {
+  constructor(private readonly clienteService: ClienteService) {}
+  @Post()
+  create(@Body() cliente: clienteDto) {
+    this.clienteService.create(cliente);
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string): clienteDto {
+    return this.clienteService.findById(id);
+  }
+
+  @Put('/:id')
+  update(@Body() cliente: clienteDto) {
+    this.clienteService.update(cliente);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string) {
+    return this.clienteService.remove(id);
+  }
+}
---
+++ src/Cliente/cliente.dto.ts
@@ -0,0 +1,15 @@
+export class clienteDto {
+  id: string;
+  nome: string;
+  cpf: string;
+  cnpj: string;
+  razaoSocial: string;
+  endereco: string;
+  cep: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  telefone: string;
+  rg: string;
+  status: boolean;
+}
