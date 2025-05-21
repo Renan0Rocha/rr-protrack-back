b960189aacea7c8571d7f67adcaf62ab1a5b5870
renanro919@gmail.com <renanro919@gmail.com>
2025-05-20 22:38:39 -0400

    feat: add Users module with controller, service, and DTO; update package dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClienteModule } from './cliente/cliente.module';
 import { ProgramaModule } from './programa/programa.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProgramaModule, ClienteModule],
+  imports: [ProgramaModule, ClienteModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
