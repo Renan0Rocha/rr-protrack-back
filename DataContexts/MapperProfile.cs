199050ac8a9d83dc9d36084ef1b7bea65c2b9c74
Eduardo Pereira França Carneiro <dugabuga6@gmail.com>
2025-07-15 20:06:57 -0400

    Refactor:  atualização do Mapper

--- DataContexts/MapperProfile.cs
+++ DataContexts/MapperProfile.cs
@@ -1,5 +1,9 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using rr_protrack_back.Dtos;
+using rr_protrack_back.Dtos.ClientesDtos;
+using rr_protrack_back.Dtos.ContratosDtos;
+using rr_protrack_back.Dtos.VendedoresDtos;
 using rr_protrack_back.Models;
 
 namespace rr_protrack_back.DataContexts
@@ -8,7 +12,38 @@ namespace rr_protrack_back.DataContexts
     {
         public MapperProfile()
         {
-            CreateMap<VendedorDto, Vendedor>().ReverseMap();
+            CreateMap<VendedorDto, Vendedor>();
+            
+            CreateMap<EnderecoDto, Endereco>().ReverseMap();
+
+            CreateMap<ClienteCreateDto, Cliente>();
+
+            CreateMap<Cliente, ClienteResponseDto>();
+
+            CreateMap<Vendedor, VendedorResponseDto>();
+
+            CreateMap<Cliente, ClienteDto>()
+            .ForMember(dest => dest.Endereco, opt => opt.MapFrom(src => src.Endereco))
+            .ForMember(dest => dest.Vendedor, opt => opt.MapFrom(src => src.Vendedor))
+            .ReverseMap();
+
+            CreateMap<ProgramaDto, Programa>().ReverseMap();
+
+            CreateMap<InsercaoDto, Insercao>().ReverseMap();
+
+            CreateMap<OrdemBlocoDto, OrdemBloco>().ReverseMap();
+
+            CreateMap<ContratoCreateDto, Contrato>();
+
+            CreateMap<OrdemBlocoContrato, OrdemBlocoContratoDto>().ReverseMap();
+
+            CreateMap<Contrato, ContratoDto>()
+            .ForMember(dest => dest.Horarios, opt => opt.MapFrom(src =>
+             src.Horarios.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(h => h.Trim()).ToList()));
+
+            CreateMap<ContratoDto, Contrato>()
+            .ForMember(dest => dest.Horarios, opt => opt.MapFrom(src => string.Join(",", src.Horarios)));
+
 
         }
     }
